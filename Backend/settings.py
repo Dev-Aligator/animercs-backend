efc24d8e74dc06f3ba2ce9baf45ab65b7a913a07
Dev-Aligator <devaligator1004@gmail.com>
2024-07-11 20:51:18 +0700

    Change the Database name to animercsdata.sqlite3

--- Backend/settings.py
+++ Backend/settings.py
@@ -111,7 +111,7 @@ WSGI_APPLICATION = 'Backend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'animercsdata.sqlite3',
     }
 }
 
